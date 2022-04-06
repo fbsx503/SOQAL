52de5d0bceadb79b15a9b17bb4780b2e4f8689df
aymanm419 <aymanm419@gmail.com>
2022-04-06 20:50:40 +0200

    Fix bug that didn't allow multiple models to run

--- araelectratf/finetune/preprocessing.py
+++ araelectratf/finetune/preprocessing.py
@@ -57,8 +57,8 @@ class Preprocessor(object):
     """Write out the dataset as tfrecords."""
     dataset_name = "_".join(sorted([task.name for task in tasks]))
     dataset_name += "_" + split
-    tfrecords_path = os.path.join(self._config.init_checkpoint, "/squadv1_dev.tfrecord")
-    metadata_path = os.path.join(self._config.init_checkpoint, "/squadv1_dev.metadata")
+    tfrecords_path = os.path.join(self._config.init_checkpoint, "squadv1_dev.tfrecord")
+    metadata_path = os.path.join(self._config.init_checkpoint, "squadv1_dev.metadata")
     batch_size = (self._config.train_batch_size if is_training else
                   self._config.eval_batch_size)
 
