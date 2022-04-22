1f8dc6e367230afd29671a441241d95e02f53985
aymanm419 <aymanm419@gmail.com>
2022-04-22 13:24:19 +0200

    Add retriever test for merged questions

---
+++ retriever/retriever_test3.py
@@ -0,0 +1,66 @@
+import json
+from TfIdfRetriever2 import *
+import argparse
+
+
+def merge_questions(qas):
+    merged_questions = ""
+    for qa in qas:
+        merged_questions += " " + qa['question']
+
+
+def accuracy_retriever(retriever, dataset):
+    with open(dataset) as f:
+        dataset = json.load(f)['data']
+    found_answers = 0
+    total_answers = 0
+    for article in dataset:
+        for paragraph in article['paragraphs']:
+            for qa in paragraph['qas']:
+                for answer in qa['answers']:
+                    print(clean_string(merge_questions(paragraph['qas'])))
+                    docs, _ = retriever.get_topk_docs_scores(merge_questions(paragraph['qas']))
+                    for doc in docs:
+                        if doc.find(answer['text']) != -1:
+                            found_answers += 1
+                            break
+                    total_answers += 1
+        print("Found answers so far: " + str(found_answers))
+        print("Total answers so far: " + str(total_answers))
+    print("####################################################")
+    print("DONE")
+    print("####################################################")
+    print("Found answers: " + str(found_answers))
+    print("Accuracy is: " + str(found_answers / total_answers))
+    print("\n\n")
+    return found_answers, total_answers
+
+
+def accuracy_TfidfRetriever(ret_path):
+    r = pickle.load(open(ret_path, "rb"))
+    dataset_path = "../data/arcd.json"
+    accuracy_retriever(r, dataset_path)
+
+
+def accuracy_Hierarchial_TfidfRetriever(ret_path, docs, ngrams_1, ngrams_2):
+    print("Evaluating Hierarchial TF-IDF Retriever ... {} DOCS, {} ngrams_1, {} ngrams_2".format(docs, ngrams_1,
+                                                                                                 ngrams_2))
+    r = pickle.load(open(ret_path, "rb"))
+    dataset_path = "../data/arcd.json"
+    accuracy_retriever(HierarchicalRetriever(r, ngrams_1, ngrams_2, docs), dataset_path)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--ret-path', help='Retriever Path', required=True)
+
+
+def main():
+    args = parser.parse_args()
+    print("Evaluating TF-IDF Retriever ...")
+    accuracy_TfidfRetriever(args.ret_path)
+    accuracy_Hierarchial_TfidfRetriever(args.ret_path, 100, 1, 4)
+    accuracy_Hierarchial_TfidfRetriever(args.ret_path, 50, 1, 4)
+
+
+if __name__ == "__main__":
+    main()
